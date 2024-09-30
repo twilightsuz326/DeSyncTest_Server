b97b56e75efb5a57a4311765836aefda2c6bb00d
twilightsuz326 <twilight.suz326@gmail.com>
2024-09-30 18:10:17 +0900

    Docker でDeSync検証環境作成するも失敗(gunicornなし)

---
+++ web1/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # the next line is required for Transfer-Encoding support in the request
+    request.environ['wsgi.input_terminated'] = True
+    headers = {}
+    for header in request.headers:
+        headers[header[0]] = header[1]
+
+    body = None
+    if request.method == 'POST':
+        body = request.get_data(as_text=True)
+
+    # ヘッダーとボディを返す
+    return jsonify({
+        'headers': headers,
+        'body': body
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
